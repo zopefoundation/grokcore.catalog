98f64637a7809a75ca9c565274265858c5a5d23b
Souheil CHELFOUH <trollfot@gmail.com>
2012-04-28 09:24:56 +0000

    Corrected import

--- src/grokcore/catalog/__init__.py
+++ src/grokcore/catalog/__init__.py
@@ -1,3 +1,3 @@
 from grokcore.catalog.interfaces import IIndexDefinition
 from grokcore.catalog.index import IndexDefinition, Field, Text, Set, Value
-from grokcore.catalog.component import IndexesClass
+from grokcore.catalog.components import IndexesClass
