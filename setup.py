91b7ec9b71d4420b72520cdd14f66ca3a969e312
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2013-05-07 14:29:14 +0200

    Preparing release 2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
 
 setup(
     name='grokcore.catalog',
-    version='2.0.dev0',
+    version='2.0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
