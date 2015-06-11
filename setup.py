bc7f9a6bd5d24ebc97669bb7e62324c9b4c1f682
Sylvain Viollon <thefunny@gmail.com>
2015-06-11 09:21:56 +0200

    Preparing release 2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
 
 setup(
     name='grokcore.catalog',
-    version='2.1.dev0',
+    version='2.1',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
