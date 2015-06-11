ccdb655da3767867c415e5920d440fa3aeed4b51
Sylvain Viollon <thefunny@gmail.com>
2015-06-11 09:22:12 +0200

    Back to development: 2.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
 
 setup(
     name='grokcore.catalog',
-    version='2.1',
+    version='2.2.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
