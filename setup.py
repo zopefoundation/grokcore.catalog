bdd9625bc83cea3e27a27aa3f745aa955a7b2616
Michael Howitz <icemac@gmx.net>
2023-07-12 09:30:37 +0200

    Back to development: 4.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 
 setup(
     name='grokcore.catalog',
-    version='4.0',
+    version='4.1.dev0',
     author='Grok Team',
     author_email='zope-dev@zope.dev',
     url='https://github.com/zopefoundation/grokcore.catalog',
